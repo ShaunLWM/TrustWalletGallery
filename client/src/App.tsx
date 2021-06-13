b3dbd0576dfa84891e24996b21be8a89f4ecdebd
ShaunLWM <shaunlimweimin@gmail.com>
2021-06-13 13:59:43 +0800

    Add token and 404 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
+import TokenPage from "./TokenPage";
 
 function App() {
 	return (
 		<Router>
 			<Switch>
-				<Route path="/">
+				<Route exact path="/">
 					<Home />
+				</Route>
+				<Route exact path="/token/:id">
+					<TokenPage />
+				</Route>
+				<Route path="*">
+					<span>404</span>
 				</Route>
 			</Switch>
 		</Router>
