77680fb1e18f83b0dab7115c750e3c1d7f24276d
ShaunLWM <shaunlimweimin@gmail.com>
2021-06-13 15:13:56 +0800

    Normalize client design colors and font

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import styled from "styled-components";
 import Home from "./Home";
 import TokenPage from "./TokenPage";
 
+const Container = styled.div`
+	height: 100%;
+	width: 100%;
+	flex: 1;
+`;
+
 function App() {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/">
-					<Home />
-				</Route>
-				<Route exact path="/token/:id">
-					<TokenPage />
-				</Route>
-				<Route path="*">
-					<span>404</span>
-				</Route>
-			</Switch>
-		</Router>
+		<Container>
+			<Router>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route exact path="/token/:id">
+						<TokenPage />
+					</Route>
+					<Route path="*">
+						<span>404</span>
+					</Route>
+				</Switch>
+			</Router>
+		</Container>
 	);
 }
 
